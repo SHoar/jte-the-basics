e9b8d0653645fee8f27b40d7679b4267a96f2b64
Hoar, Sean [USA] <Hoar_Sean@bah.com>
2021-01-06 21:24:46 -0500

    redo splunk step watcher

--- libraries/splunk/steps/splunk_step_watcher.groovy
+++ libraries/splunk/steps/splunk_step_watcher.groovy
@@ -1,4 +1,9 @@
-@Init
-void call(){
-    println "Splunk: beginning of the pipeline!"
+@BeforeStep
+void before(){
+    println "Splunk: running before the ${hookContext.library} library's ${hookContext.step} step"
+}
+
+@AfterStep
+void after(){
+    println "Splunk: running after the ${hookContext.library} library's ${hookContext.step} step"
 }
